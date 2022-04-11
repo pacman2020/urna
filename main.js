c76a4826761efb66afb1e6b0fd35de3486b62123
geraldo <geeferreirasd034@gmail.com>
2022-04-10 22:09:22 -0300

    function de add vote

--- main.js
+++ main.js
@@ -7,16 +7,25 @@ let btn = document.querySelector('#btn')
 
 let placas = [
     {
-        nome:'geraldo',
-        placa: 1
+        nome:'branco',
+        placa: 0
     },
     {
-        nome:'mateus',
-        placa: 2
+        nome:'mario',
+        placa: 0
+    },
+    {
+        nome:'alberto',
+        placa: 0
+    },
+    {
+        nome:'tales',
+        placa: 0
     }
 ]
 
 function listaPlaca() {
+    ul.innerHTML = ''
     for(x in placas){
         let li = document.createElement('li')
         li.innerHTML = `${placas[x].nome}: ${placas[x].placa}`
@@ -38,30 +47,29 @@ function save(event) {
         return 
     }
 
-    switch (candidato) {
-        case candidato === '00':
-            placas['mario'] += 1 
+    switch (candidato.value) {
+        case 'branco':
+            placas[0].placa += 1
+            console.log(placas[0].placa)
+            console.log(candidato.va)
             break;
     
-        case candidato === '01':
-            
+        case 'mario':
+            placas[1].placa += 1 
             break;
     
-        case candidato === '02':
-            
+        case 'alberto':
+            placas[2].placa += 1 
             break;
-    
-        case candidato === '03':
             
+        case 'tales':    
+            placas[3].placa += 1 
             break;
     
         default:
             break;
     }
-    console.log(cpf.value)
-    console.log(candidato.value)
+    listaPlaca()
 }
 
 btn.addEventListener('click', save, false)
-
-listaPlaca()
